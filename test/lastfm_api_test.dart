73009bfab10d7aca669ea1f1023236eabe2d16cd
lorriman <48987+lorriman@users.noreply.github.com>
2023-02-24 12:33:53 +0000

    pre ubuntu reinstall - add mockito files

---
+++ test/lastfm_api_test.dart
@@ -0,0 +1,15 @@
+import 'package:mockito/annotations.dart';
+import 'package:mockito/mockito.dart';
+import 'package:http/http.dart' as http;
+
+
+
+// Package imports:
+import 'package:test/test.dart';
+
+@GenerateMocks([http.Client])
+Future<void> main() async {
+
+
+
+}
